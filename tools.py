14fa92906980348d91c3a193bae6bc901ceebdb2
kristophtg <kristoph321@gmail.com>
2022-05-18 23:57:23 +0200

    add active/admin to admin user

--- tools.py
+++ tools.py
@@ -8,7 +8,6 @@ from backend.app.utils import get_engine, get_pwd_context
 from backend.db.schemas import Base, User
 
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("command", choices=["init_db"])
@@ -21,6 +20,6 @@ if __name__ == "__main__":
         SessionLocal = sessionmaker(bind=engine)
         db = SessionLocal()
         pwd_context = get_pwd_context()
-        user = User(username="admin", password=pwd_context.hash(config.ADMIN_PASSWORD))
+        user = User(username="admin", password=pwd_context.hash(config.ADMIN_PASSWORD), active=True, admin=True)
         db.add(user)
         db.commit()
